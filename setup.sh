8959715674590fd0286f560aec5da38d07d48175
Leo Govan <leogovan@yahoo.co.uk>
2022-03-14 09:41:37 +0000

    Change setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export DATABASE_URL="postgres://wglszhedsrwdqu:a2bcaea5caa1721d3da2cd4dd7606742e39b4e20ce5023155c47a386a6f28088@ec2-52-70-186-184.compute-1.amazonaws.com:5432/d6rtuk4oiiv30v"
+export DATABASE_URL="postgresql://127.0.0.1:5432/fs-capstone-submission"
 echo "setup.sh script executed successfully!"
