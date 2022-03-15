f34c3b8d5afd64fc06b2904a1c809cd984135dc8
Leo Govan <leogovan@yahoo.co.uk>
2022-03-15 14:31:43 +0000

    Change test to setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://127.0.0.1:5432/fs-capstone-submission"
+#export DATABASE_URL="postgresql://127.0.0.1:5432/fs-capstone-submission"
 export FLASK_APP=app.py
-export AUTH0_DOMAIN=fsnd-leogovan.eu.auth0.com
+export AUTH0_DOMAIN='fsnd-leogovan.eu.auth0.com'
 export ALGORITHMS=['RS256']
-export API_AUDIENCE=casting-agency
+export API_AUDIENCE='casting-agency'
 echo $DATABASE_URL
 echo $FLASK_APP
 echo $AUTH0_DOMAIN
