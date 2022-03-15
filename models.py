018d151b5add2aac35ed1da82a4cf59ebcba531b
Leo Govan <leogovan@yahoo.co.uk>
2022-03-15 14:25:59 +0000

    Change test to setup and models

--- models.py
+++ models.py
@@ -28,7 +28,7 @@ db = SQLAlchemy()
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SECRET_KEY"] = SECRET_KEY
+    #app.config["SECRET_KEY"] = SECRET_KEY
     #app.config["SQLALCHEMY_ECHO"] = True
     db.app = app
     db.init_app(app)
--- setup.sh
+++ setup.sh
@@ -4,4 +4,9 @@ export FLASK_APP=app.py
 export AUTH0_DOMAIN=fsnd-leogovan.eu.auth0.com
 export ALGORITHMS=['RS256']
 export API_AUDIENCE=casting-agency
+echo $DATABASE_URL
+echo $FLASK_APP
+echo $AUTH0_DOMAIN
+echo $ALGORITHMS
+echo $API_AUDIENCE
 echo "setup.sh script executed successfully!"
